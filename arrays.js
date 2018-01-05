c99f6bbc8fb8c9420ba923eed3c6c5c0653172dd
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-05 20:05:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,5 +8,5 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   var list = [];
-  list = ['element',...array];
+  list = ["element",...array];
 }
