f2e405b875fd4f45425cf09bd15753bae499d1c3
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-05 19:21:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,2 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 chocolateBars;
-
-function addElementToBeginningOfArray()
-
-function destructivelyAddElementToBeginningOfArray()
