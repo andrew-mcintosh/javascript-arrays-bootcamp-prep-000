beead298c6545949f60860dcd71d28bce993efe7
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-05 20:32:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
   chocolateBars.shift();
   return chocolateBars;
 }
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.slice(1);
+  return chocolateBars;
+}
